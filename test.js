98af43ac174067cce352ee30e2529d7ab7fcd3a0
maxlpy <maxlipengyu@gmail.com>
2015-03-08 11:43:34 -0400

    add result picture

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var subject = require('./subject.js')
 var mock = require('mock-fs');
-subject.getPositve(-202.91216694749892,446.4947790838778);
-subject.getPositve(0,774.2886301130056);
-subject.getPositve(-315.0126417167485,null);
+subject.getPositve(-529.4025947805494,126.14253419451416);
+subject.getPositve(0,417.68902470357716);
+subject.getPositve(-124.34030324220657,null);
 subject.getPositve(0,null);
